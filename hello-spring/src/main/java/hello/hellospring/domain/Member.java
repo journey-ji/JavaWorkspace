144c264ea1695aa58065cec27f42f21fec49ecd9
journey-ji <journeyji95@gmail.com>
2023-05-10 23:33:13 +0900

    테스트 케이스 작성

--- hello-spring/src/main/java/hello/hellospring/domain/Member.java
+++ hello-spring/src/main/java/hello/hellospring/domain/Member.java
@@ -1,11 +1,11 @@
-package hello.hellospring.dmoain;
+package hello.hellospring.domain;
 
 public class Member {
     private  Long id;
     private  String name;
 
     public Long getId(){
-        return id
+        return id;
     }
     public void setId(Long id){
         this.id = id;
